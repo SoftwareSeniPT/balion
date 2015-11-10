3f10116b7afee9a759805357ee4e23361a32703f
Rudy Wahyudi <rudi@softwareseni.com>
2015-11-10 17:01:48 +0700

    Add search page and details page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 var app = {
     init: function() {
         app.dropDownInit();
-        app.homeCycleInit();
+        app.detailsCycleInit();
+        app.initMap();
     },
     dropDownInit: function() {
-        jQuery('.dropdown').click(function(){
+        jQuery('.dropdown').click(function() {
             jQuery('.dropdown').not(this).removeClass('opened');
-            if(!jQuery(this).hasClass('opened')) {
+            if (!jQuery(this).hasClass('opened')) {
                 jQuery(this).addClass('opened')
             } else {
                 jQuery(this).removeClass('opened')
@@ -14,17 +15,56 @@ var app = {
         });
 
         // If dropdown clicked
-        jQuery('.dropdown li').click(function(){
+        jQuery('.dropdown li').click(function() {
             var $val = jQuery(this).text();
             jQuery(this).parents('.dropdown').find('span').text($val);
         });
-    }
+    },
+    detailsCycleInit: function() {
+        jQuery('.details-page .slideshow').cycle({
+            manualSpeed: 500,
+            slides: '.slide',
+            timeout: 1500,
+            fx: 'scrollHorz'
+        });
+    },
+    initMap: function() {
+        if(!jQuery('#content').hasClass('details')) {
+            return false;
+        }
+
+        function initialize() {
+            var lang = new google.maps.LatLng(5.962528, 80.706668);
+            var mapOptions = {
+                center: lang,
+                zoom: 16,
+                scrollwheel: false,
+                navigationControl: true,
+                mapTypeControl: false,
+                scaleControl: true,
+                draggable: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true
+            }
+            var map = new google.maps.Map(document.getElementById('map'),
+                mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: lang,
+                map: map,
+                title: 'Hiriketiya Beachhouse',
+                icon: 'http://www.beachhousehiriketiya.com/wp-content/themes/hybrid/assets/images/pin.png'
+            });
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+    },
 };
 
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
     app.init();
-    jQuery(window).resize(function(){
+    jQuery(window).resize(function() {
         app.onResize();
     });
 });
